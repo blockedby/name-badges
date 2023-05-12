c4709f2cc0f61a28c1ed38cf30849cfb0a95c497
Alex Green <kasnis12@gmail.com>
2023-05-12 14:47:28 +0300

    props changed again

--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -3,15 +3,15 @@ type NameBadgeProps = {
   greeting: string;
 };
 
-const NameBadge = ( nameProp: NameBadgeProps) => {
+const NameBadge = ( {name, greeting = 'Hello,'}: NameBadgeProps) => {
   return (
     <section className="badge">
       <header className="badge-header">
-        <h1 className="text-5xl">{nameProp.greeting}</h1>
+        <h1 className="text-5xl">{greeting}</h1>
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">{nameProp.name}</p>
+        <p className="badge-name">{name}</p>
       </div>
       <footer className="badge-footer" />
     </section>

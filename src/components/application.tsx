8cc5aea6a234a1f2f2d08e38e82d157e36b0d20a
Alex Green <kasnis12@gmail.com>
2023-05-12 14:19:13 +0300

    name props

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -3,7 +3,7 @@ import NameBadge from './name-badge';
 
 const Application = () => (
   <main className="application">
-    <NameBadge />
+    <NameBadge name='Sasha' greeting='Hello dear' />
   </main>
 );
 
--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -1,12 +1,17 @@
-const NameBadge = () => {
+type NameBadgeProps = {
+  name: string;
+  greeting: string;
+};
+
+const NameBadge = ( nameProp: NameBadgeProps) => {
   return (
     <section className="badge">
       <header className="badge-header">
-        <h1 className="text-5xl">HELLO</h1>
+        <h1 className="text-5xl">{nameProp.greeting}</h1>
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">Steve</p>
+        <p className="badge-name">{nameProp.name}</p>
       </div>
       <footer className="badge-footer" />
     </section>
